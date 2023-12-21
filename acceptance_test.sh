844c024f99935211bf236da3fc0ccc91d3a9e0d2
shieldlines <152596838+shieldlines@users.noreply.github.com>
2023-12-21 18:39:38 +0100

    Update acceptance_test.sh

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-test $(curl localhost:8765/sum?a=20\&b=80) -eq 100
+
+# Set the HOME environment variable
+export HOME=/var/lib/jenkins
+
+# Use command substitution to capture the result of curl
+result=$(curl -s localhost:8765/sum?a=20\&b=80)
+
+# Convert the result to an integer for numeric comparison
+result_int=$((result))
+
+# Check if the result is equal to 100
+if [ "$result_int" -eq 100 ]; then
+    echo "Condition is true"
+else
+    echo "Condition is false"
+fi
